ec5982478733a3695939db440b08ae056416af51
AlekseiFom <alurfom@gmail.com>
2026-03-30 02:34:04 +0500

    lesson3: классное решение задач по классам

---
+++ lesson_03/lesson_3_task_2.py
@@ -0,0 +1,32 @@
+from smartphone import Smartphone
+
+catalog = [
+    Smartphone(
+        "iphone",
+        "17pro",
+        "+79123456789"
+    ),
+    Smartphone(
+        "Samsung",
+        "A15",
+        "+79234567899"
+    ),
+    Smartphone(
+        "Redmi",
+        "11Light",
+        "+79876543211"
+    ),
+    Smartphone(
+        "Xaomi",
+        "Note 05",
+        "-79856942255"
+    ),
+    Smartphone(
+        "Nokia",
+        "3210",
+        "+79224790827"
+    )
+]
+
+for item in catalog:
+    print(f"{item.mark} - {item.model}. {item.phone}")
